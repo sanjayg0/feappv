628445ef904a173affd531be59da8e3bac1e22b0
Sanjay Govindjee <sg@Sanjays-MacBook-Pro2018.local>
2019-06-11 09:22:23 -0700

    Fix typo on the definitions of ht1, ht2, ht3

--- include/integer4/hdata.h
+++ include/integer4/hdata.h
@@ -1,3 +1,3 @@
 
-      integer         nh1,nh2,nh3,nt1,nt2,nt3                     !int 4
-      common /hdata/  nh1,nh2,nh3,nt1,nt2,nt3
+      integer         nh1,nh2,nh3,ht1,ht2,ht3                     !int 4
+      common /hdata/  nh1,nh2,nh3,ht1,ht2,ht3

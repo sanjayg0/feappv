7c1f0d5f7d4731921c7cd1eb0d302e2ad19f014c
R.L.Taylor <rlt@ce.berkeley.edu>
2020-01-14 16:08:31 -0800

    New files for Lagrange multipliers (mostly)

--- program/pupdate.f
+++ program/pupdate.f
@@ -1,16 +1,16 @@
 !$Id:$
-      subroutine pupdate(id,f,u,urate,du,fdyn,isw)
+      subroutine pupdate(eq,f,u,urate,du,fdyn,isw)
 
 !      * * F E A P * * A Finite Element Analysis Program
 
-!....  Copyright (c) 1984-2019: Regents of the University of California
+!....  Copyright (c) 1984-2020: Regents of the University of California
 !                               All rights reserved
 
 !-----[--.----+----.----+----.-----------------------------------------]
 !      Purpose: Static and Dynamic UPDATE algorithms for F E A P
 
 !      Inputs:
-!       id (ndf,*)   - ID-array (vector storage)
+!       eq (ndf,*)   - Active equation numbers
 !       f  (nneq,2)  - Nodal force/displ: 1 = current; 2 = previous
 !       u (3*nneq)   - Displacement vectors
 !       urate(nneq,*)- Rate vectors fixed by ALGO
@@ -41,55 +41,58 @@
       include  'comblk.h'
 
       logical       :: fdyn
-      integer       :: j, n, nneq2,isw, nod, id(*)
+      integer       :: j, n, nneq2,isw, nod, eq(*)
       real (kind=8) :: ub,vnorm,anorm
       real (kind=8) :: f(nneq,*),u(*),urate(nneq,*),du(*)
 
       save
 
 !     Update solution vectors to begin a step
-
       nneq2 = nneq + nneq
 
       if(isw.eq.1) then
 
-!       Compute and output norms
-
-        if(pfr) then
-        vnorm = 0.0d0
-        do n = 1,nneq
-          vnorm = vnorm + urate(n,1)*urate(n,1)
-        end do
-        vnorm = sqrt(vnorm)
-        anorm = 0.0d0
-        if(noi.eq.1 .or. noi.eq.3 .or.
-     &     noi.eq.4 .or. noi.eq.5 .or.
-     &     noi.eq.8               ) then
-          do n = 1,nneq
-            anorm = anorm + urate(n,2)*urate(n,2)
-          end do
-          anorm = sqrt(anorm)
+!       Initialize Lagrange multiplier variables
+        if(np(210).ne.0) then
+         call uplagm(du,hr(np(213)),mr(np(210)),mr(np(32)),mr(np(33)),1)
         endif
-        if(prnt.and.ior.lt.0) write(*,2000) vnorm,anorm
-        write(iow,2000) vnorm,anorm
+
+!       Compute and output norms
+        if(pfr) then
+          if(noi.gt.0) then
+            vnorm = 0.0d0
+            do n = 1,nneq
+              vnorm = vnorm + urate(n,1)*urate(n,1)
+            end do
+            vnorm = sqrt(vnorm)
+            anorm = 0.0d0
+            do n = 1,nneq
+              anorm = anorm + urate(n,2)*urate(n,2)
+            end do
+            anorm = sqrt(anorm)
+            if(prnt.and.ior.lt.0) write(*,2000) vnorm,anorm
+            write(iow,2000) vnorm,anorm
+          endif
         endif
 
 !       Update rate terms
 
-!       Generalized Newmark updates
-
+!       Newmark updates
         if(noi.eq.1) then
 
           call dyna01(u,urate,nneq,1)
 
-!       SSpj updates
-
+!       GNpj updates
         elseif(noi.eq.2) then
 
           call dyna02(u,urate,nneq,1)
 
-!       USER:  User generated routine
+!       SSpj updates
+        elseif(noi.eq.3) then
 
+          call dyna03(u,urate,nneq,1)
+
+!       USER:  User generated routine
         elseif(noi.eq.-1) then
 
           call udynam(u(nneq2+1),u,urate,nneq,1)
@@ -97,20 +100,17 @@
         endif
 
 !     UPDATES WITHIN A TIME STEP
-
       elseif(isw.eq.2) then
 
 !       STEP 1: Update displacement and its increments within step.
-
         nod = 0
         do n = 1,nneq
           nod = nod + 1
-          j = id(n)
+          j   = eq(n)
           if (j.gt.0) then
 
 !           For active dof compute values from solution
 !           where 'du(j)' is increment of 'u' for active dof 'j'.
-
             u(n)       = u(n)      + cc1*du(j)
             u(n+nneq)  = u(n+nneq) + cc2*du(j)
             u(n+nneq2) =                 du(j)
@@ -118,11 +118,9 @@
           else
 
 !           Compute values from forced inputs for fixed dof
-
             ub         = f(n,1)
 
 !           Incremental boundary solution
-
             if(cc3.ne.0.0d0) then
               u(n+nneq2) = (ub - u(n))*cc3
             else
@@ -136,24 +134,30 @@
           endif
         end do
 
-!       STEP 2: For Dynamics, update rate terms [urate-vectors]
+!       Update Lagrange multiplier variables
+        if(np(210).ne.0) then
+         call uplagm(du,hr(np(213)),mr(np(210)),mr(np(32)),mr(np(33)),2)
+        endif
 
+!       STEP 2: For Dynamics, update rate terms [urate-vectors]
         if(fdyn) then
 
-!         Generalized Newmark updates
-
+!         Newmark updates
           if(noi.eq.1) then
 
             call dyna01(u(nneq2+1),urate,nneq,2)
 
-!         SSpj Updates
-
+!         GNpj Updates
           elseif(noi.eq.2) then
 
             call dyna02(u(nneq2+1),urate,nneq,2)
 
-!         USER:  User generated routine
+!         SSpj Updates
+          elseif(noi.eq.3) then
 
+            call dyna03(u(nneq2+1),urate,nneq,2)
+
+!         USER:  User generated routine
           elseif(noi.eq.-1) then
 
             call udynam(u(nneq2+1),u,urate,nneq,2)
@@ -162,25 +166,26 @@
         endif
 
 !     BACKUP SOLUTION VECTORS: Reinitiate a transient step
-
       elseif(isw.eq.3) then
 
         if(fdyn)then
 
-!         Generalize Newmark updates
-
+!         Newmark updates
           if(noi.eq.1) then
 
             call dyna01(u(nneq+1),urate,nneq,3)
 
-!         SSpj updates
-
+!         GNpj updates
           elseif(noi.eq.2) then
 
             call dyna02(u(nneq+1),urate,nneq,3)
 
-!         USER:  User generated routine
+!         SSpj updates
+          elseif(noi.eq.3) then
 
+            call dyna03(u(nneq+1),urate,nneq,3)
+
+!         USER:  User generated routine
           elseif(noi.eq.-1) then
 
             call udynam(u(nneq2+1),u,urate,nneq,3)
@@ -190,21 +195,18 @@
         endif
 
 !       Back up solution vectors: u(*)
-
         vnorm = 1.d0/cc2
         do n = 1,nneq
           ub = u(n+nneq)*vnorm
-          j  = id(n)
+          j  = eq(n)
 
 !         Compute values from current solution
-
           if (j.gt.0) then
             u(n)       = u(n) - ub
             u(n+nneq)  = 0.
             u(n+nneq2) = 0.
 
-!           Compute values from forced inputs
-
+!         Compute values from forced inputs
           else
             du(neq-j)  = 0.
             u(n+nneq2) = 0.
@@ -213,6 +215,11 @@
             u(n)       = f(n,2)
           endif
         end do
+
+!       Back up Lagrange multiplier variables
+        if(np(210).ne.0) then
+         call uplagm(du,hr(np(213)),mr(np(210)),mr(np(32)),mr(np(33)),3)
+        endif
       endif
 
 !     Formats

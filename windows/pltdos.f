9be202c37cf2cd2a4e4460f13d4e29ba77cbeb57
Sanjay Govindjee <s_g@berkeley.edu>
2026-05-09 22:13:09 -0700

    Repair messed up pltdos.f

--- windows/pltdos.f
+++ windows/pltdos.f
@@ -35,44 +35,6 @@
       type(windowconfig) :: myscreen
       type(qwinfo)       :: winfo,frmwindow
       logical            :: status
-      use        IFQWIN
-
-      implicit   none
-
-      type(windowconfig)  :: textscreen
-      logical             :: vstatus
-
-      save
-
-!     Open Child Console Window for text inputs
-
-      open (unit = 5, file = 'CON')
-
-!     Get Current Input Data Window Size
-
-      vstatus  = getwindowconfig(textscreen)
-
-!     Set the x & y pixels & font to give 80 x 50 text window
-
-      textscreen.numxpixels   =  800
-      textscreen.numypixels   =  800
-      textscreen.numtextcols  =  -1
-      textscreen.numtextrows  =  -1
-      textscreen.fontsize     = #000A0010
-      textscreen.numcolors    =  -1
-      textscreen.title =
-     &"Finite  Element  Analysis  Program - Personal  Version"C
-
-      textscreen.mode = qwin$scrolldown
-
-      vstatus = setwindowconfig(textscreen)
-      if(.not.vstatus) vstatus = setwindowconfig(textscreen)
-
-      vstatus = deletemenuqq(4, 0)
-
-      vstatus = displaycursor($gcursoron)
-
-      end subroutine pwopn
       integer(2)         :: numfonts
       integer            :: uchild
 
